f041d763287c8b370974be2223ba9082f9af2102
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2026-04-08 12:56:58 -0500

    refactor(containercreate): simplify workdir flag with direct value
    Removed unnecessary option handling for workdir flag and use direct value instead of O.FromPredicate/O.Fold pattern.

--- internal/containercreate/args.go
+++ internal/containercreate/args.go
@@ -5,7 +5,6 @@ import (
 
 	A "github.com/IBM/fp-go/v2/array"
 	F "github.com/IBM/fp-go/v2/function"
-	O "github.com/IBM/fp-go/v2/option"
 )
 
 const containerBinary = "container"
@@ -30,13 +29,7 @@ func RenderCommand(rinput ResolvedInput) CommandSpec {
 			"--env",
 			fmt.Sprintf("CRUSH_GLOBAL_DATA=%s", DataTarget),
 		}),
-		A.Concat(F.Pipe1(
-			O.FromPredicate(func(s string) bool { return s != "" })(rinput.Workdir),
-			O.Fold(
-				func() []string { return []string{} },
-				func(w string) []string { return []string{"--workdir", w} },
-			),
-		)),
+		A.Concat([]string{"--workdir", rinput.Workdir}),
 		A.Push(rinput.ImageName),
 		func(args []string) CommandSpec {
 			return CommandSpec{Bin: containerBinary, Args: args}
